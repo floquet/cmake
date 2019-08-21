0d550c6c337a7100bcd29a67b9678c03cfc9a991
Daniel Topa <dantopa@gmail.gov>
2019-08-21 16:43:17 -0600

    proj 001

---
+++ demo/proj-001/bad-hal.cpp
@@ -0,0 +1,10 @@
+#include <cstdlib> # EXIT_SUCCESS
+#include <iostream>
+#include <string>
+
+  std::string say_hello() { return std::string("I’m sorry, Dave. I’m afraid I can’t do that."); }
+
+  int main() {
+      std::cout << say_hello() << std::endl;
+  return EXIT_SUCCESS;
+}
