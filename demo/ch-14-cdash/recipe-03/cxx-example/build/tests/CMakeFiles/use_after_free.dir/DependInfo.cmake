b6bf59078a90e088faba514500e17997482c1222
Daniel Topa <dantopa@lanl.gov>
2019-08-30 06:00:34 +0000

    recipe-03 fails too

---
+++ demo/ch-14-cdash/recipe-03/cxx-example/build/tests/CMakeFiles/use_after_free.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mightybuilder/repos/github/cmake/demo/ch-14-cdash/recipe-03/cxx-example/tests/use_after_free.cpp" "/home/mightybuilder/repos/github/cmake/demo/ch-14-cdash/recipe-03/cxx-example/build/tests/CMakeFiles/use_after_free.dir/use_after_free.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/mightybuilder/repos/github/cmake/demo/ch-14-cdash/recipe-03/cxx-example/build/src/CMakeFiles/buggy.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ demo/ch-14-cdash/recipe-03/cxx-example/build_no_asan/tests/CMakeFiles/leaky.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leaky.dir/leaky.cpp.o"
+  "leaky.pdb"
+  "leaky"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/leaky.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
