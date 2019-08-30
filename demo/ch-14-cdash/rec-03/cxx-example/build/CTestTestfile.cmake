6a8418647390d32a4633f960430e18e1e26502b4
Daniel Topa <dantopa@lanl.gov>
2019-08-30 05:46:48 +0000

    03 fails

---
+++ demo/ch-14-cdash/rec-03/cxx-example/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/mightybuilder/repos/github/cmake/demo/ch-14-cdash/rec-03/cxx-example
+# Build directory: /home/mightybuilder/repos/github/cmake/demo/ch-14-cdash/rec-03/cxx-example/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("tests")
---
+++ demo/ch-14-cdash/rec-03/cxx-example/build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/mightybuilder/repos/github/cmake/demo/ch-14-cdash/rec-03/cxx-example/tests
+# Build directory: /home/mightybuilder/repos/github/cmake/demo/ch-14-cdash/rec-03/cxx-example/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(leaky "/home/mightybuilder/repos/github/cmake/demo/ch-14-cdash/rec-03/cxx-example/build/tests/leaky")
+add_test(use_after_free "/home/mightybuilder/repos/github/cmake/demo/ch-14-cdash/rec-03/cxx-example/build/tests/use_after_free")
